6b994bebb79f25026c674f137912b2679207d311
Kai <shadowkai0121@gmail.com>
2019-07-23 10:30:50 +0800

    休息

---
+++ tool.js
@@ -0,0 +1,10 @@
+function commutation(str) {
+    if (str) {
+        document.write("<hr>");
+        document.write("===" + str + "===");
+        document.write("<br>");
+    }
+    else {
+        document.write("<br>");
+    }
+}
