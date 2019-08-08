c77b5c8c8d4337d3bf3276bca79852fe22a7b48d
yiwen902 <yiwen902@gmail.com>
2019-08-08 18:00:15 +0800

    下課

--- 190808/Lab_XPath/demo.php
+++ 190808/Lab_XPath/demo.php
@@ -1,18 +1,14 @@
 <?php
 header("content-type: text/html; charset=utf-8");
 
-// 1. 初始設定
 $ch = curl_init();
 
-// 2. 設定 / 調整參數
 curl_setopt($ch, CURLOPT_URL, "https://www.taipower.com.tw/TC/news2.aspx?mid=225");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
-// 3. 執行，取回 response 結果
 $pageContent = curl_exec($ch);
 
-// 4. 關閉與釋放資源
 curl_close($ch);
 
 $doc = new DOMDocument();
@@ -23,5 +19,5 @@ $xpath = new DOMXPath($doc);
 $entries = $xpath->query('//*[@id="news_box3"]/div[2]/ul/li');
 foreach ($entries as $entry) {
     $title = $xpath->query("./a/div/h3", $entry);
-    echo "Title：" . $title->item(0)->nodeValue . "<br>";
+    var_dump($title->item(0));
 }
